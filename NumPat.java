41d586414a88fdf50fa50398d0d958c1c640793b
hi-roshani <roshaniwagh00@gmail.com>
2024-04-24 12:39:35 +0530

    First commit

--- NumPat.java
+++ NumPat.java
@@ -2,14 +2,17 @@ public class NumPat {
     public static void main(String []args)
     {
         int i, j;
-        for( i=3 ; i<8 ; i++ ) {
+        for( i=3 ; i<8 ; i++ )
+        {
             for (j = 1; j <= i; j++) {
-                if ((i + j) % 2 == 1) {
+                if ((i + j) % 2 == 1)
+                {
                     System.out.print("1");
-                } else {
+                }
+                else
+                {
                     System.out.print("0");
                 }
-
             }
             System.out.println();
         }
---
+++ out/production/Demo-Java/README.md
@@ -0,0 +1,2 @@
+# Demo-Java
+I have created this repository to practice flow of github
