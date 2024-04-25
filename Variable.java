b84b311c33b7dcb7c3a799c1a61a83ce8f19b92f
hi-roshani <roshaniwagh00@gmail.com>
2024-04-25 16:23:27 +0530

    Variables

---
+++ Variable.java
@@ -0,0 +1,16 @@
+public class Variable {
+
+    public String i= "I am instance variable";
+
+    static String s = "I am static variable";
+
+    public static void main(String []args)
+    {
+        String l  = "I am local variable";
+        Variable iv = new Variable();
+        System.out.println(iv.i);
+        System.out.println(l);
+        System.out.println(s);
+    }
+
+}
