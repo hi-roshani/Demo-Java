3b084a51145e5ba90c2fd5625f887fef058de5d3
hi-roshani <roshaniwagh00@gmail.com>
2024-05-03 10:07:27 +0530

    Single Inheritance Practice

---
+++ SingBase.java
@@ -0,0 +1,30 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class SingBase {
+    private Integer a, b;
+
+    public void vals(Integer a, Integer b){
+        this.a = a;
+        this.b = b;
+    }
+
+
+    public Integer getA(){
+        return a;
+    }
+    public void setA(Integer a)
+    {
+        this.a = a;
+    }
+
+
+    public Integer getB(){
+        return b;
+    }
+    public void setB(Integer b){
+        this.b = b;
+    }
+
+}
---
+++ SingSub.java
@@ -0,0 +1,30 @@
+import javax.swing.text.html.parser.Entity;
+import java.util.Scanner;
+
+public class SingSub extends SingBase
+{
+    public static Integer take()
+    {
+        SingBase sb = new SingBase();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first value: ");
+        sb.setA(sc.nextInt());
+        System.out.println("Enter second value: ");
+        sb.setB(sc.nextInt());
+
+        Integer a= sb.getA();
+        Integer b = sb.getB();
+
+        return a + b;
+
+    }
+
+
+    public static void main(String[] args)
+    {
+        String c = String.valueOf(take());
+        System.out.println("Addition is: " + c );
+
+    }
+
+}
