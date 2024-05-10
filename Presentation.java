de724e6bd519c44fa53639d5f0bbb4594f15e6fa
hi-roshani <roshaniwagh00@gmail.com>
2024-05-10 16:12:08 +0530

    Map using streams

--- Presentation.java
+++ Presentation.java
@@ -1,8 +1,12 @@
 import day13.*;
 
+import java.util.Map;
+
 public class Presentation {
     public static void main(String[] args){
         MapStudent mapStudent = new MapStudent();
+        Map<Integer, String> studentDTOList = mapStudent.studentData();
+        System.out.println(studentDTOList);
         mapStudent.studentData();
     }
 }
--- day13/MapStudent.java
+++ day13/MapStudent.java
@@ -4,39 +4,38 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MapStudent {
     public Map<Integer, String> studentData(){
         List<StudentDTO> studentData = new ArrayList<>();
-        Map<Integer, String> tutor = new HashMap<>();
+        Map<Integer, String> roll = new HashMap<>();
 
 
         StudentDTO sDto = new StudentDTO();
         sDto.setName("Vidya"); sDto.setCourse("Python"); sDto.setCgpa(8.1f);
         studentData.add(sDto);
-        tutor.put(1, sDto.getCourse());
+        roll.put(1, sDto.getCourse());
 
         sDto = new StudentDTO();
         sDto.setName("Ram"); sDto.setCourse("Kotlin"); sDto.setCgpa(9.7f);
         studentData.add(sDto);
-        tutor.put(2, sDto.getCourse());
+        roll.put(2, sDto.getCourse());
 
 
         sDto = new StudentDTO();
         sDto.setName("Pratiksha"); sDto.setCourse("Kotlin"); sDto.setCgpa(7.7f);
         studentData.add(sDto);
-        tutor.put(3, sDto.getCourse());
+        roll.put(3, sDto.getCourse());
 
 
         sDto = new StudentDTO();
         sDto.setName("Preeti"); sDto.setCourse("Python"); sDto.setCgpa(8.5f);
         studentData.add(sDto);
-        tutor.put(4, sDto.getCourse());
+        roll.put(4, sDto.getCourse());
 
-        Stream<String> stream = tutor.values().stream();
+
+        Stream<Map.Entry<Integer, String>> stream = roll.entrySet().stream();
         System.out.println(stream);
 
         return studentData();
